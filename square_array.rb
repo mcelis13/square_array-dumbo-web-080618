026b51f792882773b780a39ce582904020755863
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 00:34:42 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,15 @@
 def square_array(array)
   newArray  = []
+  # array each does not save the value of whatever you are doing to 
+  # you array index so you have to push it into a new array or save it
   array.each do |index|
     newArray.push(index ** 2)
   end
   return newArray
 end
+
+# array.collect will save the newArray if you assign it to a variable
+# def square_array(array){
+  newArray = array.collect {|index| index ** 2}
+}
+
